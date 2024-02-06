9348a257e7d2ee8032be32d17139739790bbbe8b
Udeh Ndukwe <udehndukwe@gmail.com>
2024-02-06 00:39:05 -0500

    added scripts

---
+++ powershell/VM Scripts/Remove-VM.ps1
@@ -0,0 +1,4 @@
+﻿param([Parameter(Mandatory)]$Name)
+
+Remove-VM $Name -Confirm:$false
+Remove-Item -Path "C:\Prod\VHDs\$Name.vhdx" -Confirm:$false -Recurse
---
+++ powershell/VM Scripts/newnat(test).ps1
@@ -0,0 +1,13 @@
+﻿param
+(
+[Parameter(Mandatory)]$SwitchName,
+[Parameter(Mandatory)]$IPAddress,
+[Parameter(Mandatory)]$IPAddress2,
+[Parameter(Mandatory)]$NATNAME
+)
+
+$VMSWitch = New-VMSwitch -Name $SwitchName -SwitchType Internal
+$IF = Get-NetAdapter -Name "vEthernet ($SwitchName)" | Select-Object -expand InterfaceIndex
+
+New-NetIPAddress -IPAddress $IPAddress -PrefixLength 24 -InterfaceIndex $IF
+New-NetNat -Name $NATNAME -InternalIPInterfaceAddressPrefix $IPAddress2"/24"
