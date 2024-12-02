c80970eef128dfdb22b769fd3cb31e4e2bb9ffe5
Udeh Ndukwe <udehndukwe@gmail.com>
2024-12-01 20:57:31 -0500

    New additions

---
+++ PowerShell/IntuneScripts/SetCertificateUserIDMapping.ps1
@@ -0,0 +1,105 @@
+$URI = "https://graph.microsoft.com/v1.0/users?`$select=authorizationinfo,displayName,id,userPrincipalName"
+
+Invoke-MgGraphRequest -method GET -Uri $URI | Select -expand Value 
+
+
+# Set Value
+
+function Set-CertificateUserIDMapping {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$UserID
+    )
+
+    $certificates = Import-CSV $path
+
+    $Issuer = "CN=ad-CA01-CA,DC=ad,DC=relrepairs,DC=com"
+    foreach ($certificate in $certificates) {
+    
+        $certificate | Where UPN -eq "$UserID"
+        $SerialNumber = $cert.SerialNumber
+        $Info = "X509:<I>$Issuer<SR>$SerialNumber"    
+
+
+        Invoke-MGGraphRequest -Method patch -Uri 'https://graph.microsoft.com/v1.0/users/$UserID/?`$select=authorizationinfo' -OutputType PSObject -Headers @{'ConsistencyLevel' = 'eventual' } -Body $params
+    }
+
+}
+
+
+function Get-EntraAuthorizationInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$EntraUser
+    )
+    Invoke-MGGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$EntraUser/?`$select=authorizationinfo" | Select -expand authorizationInfo | Select -expand certificateUserIds
+}
+
+function f1 {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$UserID,
+        [switch]$Serial,
+        [switch]$SKI
+    )
+    #$ID = Invoke-MgGraphRequest -uri "https://graph.microsoft.com/v1.0/users/$UserID" | Select -expand Id
+    if ($Serial) {
+        $certificates = Import-CSV $path
+        $Issuer = "DC=com,DC=relrepairs,DC=ad,CN=ad-CA01-CA"
+        $cert = @()
+        foreach ($certificate in $certificates) {
+            $cert += $certificate | Where-Object UPN -eq "$UserID"
+        }
+        $SerialNumber = $cert | Where-Object SerialNumber | Select-Object -ExpandProperty SerialNumber
+        $Info = @()
+        foreach ($serial in $SerialNumber) {
+            $Info += "X509:<I>$Issuer<SR>$serial" 
+        }
+    
+        $params = @{
+            authorizationInfo = @{
+                certificateUserIds = @(
+                    foreach ($item in $info) {
+                        $item
+                    }   
+                )
+            }
+        }
+    }
+
+    if ($SKI) {
+        $UserName = whoami -upn
+        $mycert = ls cert:\CurrentUser\My\ | Where Subject -like "*E=$username*"
+        $SubjectKeyID = $mycert.Extensions | Where { $_.Oid.FriendlyName -eq "Subject Key Identifier" }  | Select -expand SubjectKeyIdentifier    
+
+        $params = @{
+            authorizationInfo = @{
+                certificateUserIds = @(
+                    "X509:<SKI>$SubjectKeyID"
+                )
+            }
+        }
+    }
+
+    Invoke-MgGraphRequest -Method PATCH -Uri "https://graph.microsoft.com/v1.0/users/$UserID" -Body $params
+    Get-EntraAuthorizationInfo -EntraUser $UserID
+    
+}
+
+
+
+
+
+function Get-EntraAuthorizationInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$EntraUser
+    )
+    Invoke-MGGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$EntraUser/?`$select=authorizationinfo" | Select -expand authorizationInfo | Select -expand certificateUserIds
+}
+
+
