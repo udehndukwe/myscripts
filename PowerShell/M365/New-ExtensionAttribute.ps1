08cb9bc5d49966c50907b5ec9033d678a3e5a0b8
Udeh Ndukwe <udehndukwe@gmail.com>
2024-12-11 12:53:35 -0500

    Modified 'New-ExtensionAttribute' script. Added ShouldProcess logic.

--- PowerShell/M365/New-ExtensionAttribute.ps1
+++ PowerShell/M365/New-ExtensionAttribute.ps1
@@ -1,5 +1,5 @@
 function New-ExtensionAttribute {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='High')]
     param (
         [Parameter()]
         [string]$DeviceID,
@@ -8,34 +8,14 @@ function New-ExtensionAttribute {
         [switch]$ClearAll
     )
 
-
     $URI = "https://graph.microsoft.com/v1.0/devices/$DeviceID"
 
     if ($ClearAll) {
-        $body = @{
-            extensionAttributes = @{}
-        }
-        1..15 | ForEach-Object { $body.extensionAttributes["ExtensionAttribute$_"] = $null }
-        Invoke-MgGraphRequest -Method PATCH -Uri $URI -Body $body -ErrorAction Stop
-
-        $URI2 = "https://graph.microsoft.com/v1.0/devices/$($DeviceID)?`$select=extensionAttributes"
-        $filteredDevice = Invoke-MgGraphRequest -Method GET -Uri $URI2 -ErrorAction Stop
-
-        Write-Verbose "The following extensionAttributes are set on device: $($device.displayName)" -Verbose
-        $filteredDevice.extensionAttributes.GetEnumerator() | Where-Object { $_.Value -ne $null } | ForEach-Object { @{ $_.Key = $_.Value } }
-    }
-    else {
-
-        try {
-            $device = Invoke-MgGraphRequest -Method GET -Uri $URI -ErrorAction Stop
-
+        if ($PSCmdlet.ShouldProcess("Device $DeviceID", "Clear all extension attributes")) {
             $body = @{
                 extensionAttributes = @{}
             }
-            $body.extensionAttributes.$ExtensionAttribute = $ExtensionAttributeValue
-
-            Write-Verbose "Setting $ExtensionAttribute to '$ExtensionAttributeValue' for device: $($device.displayName)" -Verbose
-
+            1..15 | ForEach-Object { $body.extensionAttributes["ExtensionAttribute$_"] = $null }
             Invoke-MgGraphRequest -Method PATCH -Uri $URI -Body $body -ErrorAction Stop
 
             $URI2 = "https://graph.microsoft.com/v1.0/devices/$($DeviceID)?`$select=extensionAttributes"
@@ -44,10 +24,31 @@ function New-ExtensionAttribute {
             Write-Verbose "The following extensionAttributes are set on device: $($device.displayName)" -Verbose
             $filteredDevice.extensionAttributes.GetEnumerator() | Where-Object { $_.Value -ne $null } | ForEach-Object { @{ $_.Key = $_.Value } }
         }
+    }
+    else {
+        try {
+            $device = Invoke-MgGraphRequest -Method GET -Uri $URI -ErrorAction Stop
+
+            if ($PSCmdlet.ShouldProcess("Device $DeviceID", "Set $ExtensionAttribute to '$ExtensionAttributeValue'")) {
+                $body = @{
+                    extensionAttributes = @{}
+                }
+                $body.extensionAttributes.$ExtensionAttribute = $ExtensionAttributeValue
+
+                Write-Verbose "Setting $ExtensionAttribute to '$ExtensionAttributeValue' for device: $($device.displayName)" -Verbose
+
+                Invoke-MgGraphRequest -Method PATCH -Uri $URI -Body $body -ErrorAction Stop
+
+                $URI2 = "https://graph.microsoft.com/v1.0/devices/$($DeviceID)?`$select=extensionAttributes"
+                $filteredDevice = Invoke-MgGraphRequest -Method GET -Uri $URI2 -ErrorAction Stop
+
+                Write-Verbose "The following extensionAttributes are set on device: $($device.displayName)" -Verbose
+                $filteredDevice.extensionAttributes.GetEnumerator() | Where-Object { $_.Value -ne $null } | ForEach-Object { @{ $_.Key = $_.Value } }
+            }
+        }
         catch {
             Write-Error "An error occurred: $_"
         }
-    
         finally {
             Write-Verbose "Finished processing New-ExtensionAttribute function." -Verbose
         }
