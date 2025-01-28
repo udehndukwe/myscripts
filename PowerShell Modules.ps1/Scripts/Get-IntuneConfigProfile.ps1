fe5f7c89d8f309eab9cafcd93b6dbd4aaefe1c20
Udeh Ndukwe <udehndukwe@gmail.com>
2025-01-28 08:22:28 -0500

    Added script for Entra CBA

--- PowerShell Modules.ps1/Scripts/Get-IntuneConfigProfile.ps1
+++ PowerShell Modules.ps1/Scripts/Get-IntuneConfigProfile.ps1
@@ -2,7 +2,7 @@ function Get-IntuneConfigProfile {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [ValidateSet("Windows", "macOS", "Android", "iOS")]
+        [ValidateSet('Windows', 'macOS', 'Android', 'iOS')]
         [string]$Platform,
         [switch]$All,
         [switch]$AllProperties
@@ -12,48 +12,48 @@ function Get-IntuneConfigProfile {
 
   
         $Windows = @(
-            "#microsoft.graph.windows10GeneralConfiguration",
-            "#microsoft.graph.windows10CustomConfiguration",
-            "#microsoft.graph.windowsDomainJoinConfiguration",
-            "#microsoft.graph.windowsWifiEnterpriseEAPConfiguration",
-            "#microsoft.graph.windows81TrustedRootCertificate",
-            "#microsoft.graph.windowsUpdateForBusinessConfiguration",
-            "#microsoft.graph.windows81SCEPCertificateProfile",
-            "#microsoft.graph.windowsDeliveryOptimizationConfiguration",
-            "#microsoft.graph.windowsKioskConfiguration",
-            "#microsoft.graph.editionUpgradeConfiguration",
-            "#microsoft.graph.windowsHealthMonitoringConfiguration",
-            "#microsoft.graph.sharedPCConfiguration"
+            '#microsoft.graph.windows10GeneralConfiguration',
+            '#microsoft.graph.windows10CustomConfiguration',
+            '#microsoft.graph.windowsDomainJoinConfiguration',
+            '#microsoft.graph.windowsWifiEnterpriseEAPConfiguration',
+            '#microsoft.graph.windows81TrustedRootCertificate',
+            '#microsoft.graph.windowsUpdateForBusinessConfiguration',
+            '#microsoft.graph.windows81SCEPCertificateProfile',
+            '#microsoft.graph.windowsDeliveryOptimizationConfiguration',
+            '#microsoft.graph.windowsKioskConfiguration',
+            '#microsoft.graph.editionUpgradeConfiguration',
+            '#microsoft.graph.windowsHealthMonitoringConfiguration',
+            '#microsoft.graph.sharedPCConfiguration'
         )
 
         $macOS = @(
-            "#microsoft.graph.macOSCustomConfiguration",
-            "#microsoft.graph.macOSGeneralDeviceConfiguration",
-            "#microsoft.graph.macOSCustomAppConfiguration",
-            "#microsoft.graph.macOSScepCertificateProfile",
-            "#microsoft.graph.macOSTrustedRootCertificate",
-            "#microsoft.graph.macOSSoftwareUpdateConfiguration"
+            '#microsoft.graph.macOSCustomConfiguration',
+            '#microsoft.graph.macOSGeneralDeviceConfiguration',
+            '#microsoft.graph.macOSCustomAppConfiguration',
+            '#microsoft.graph.macOSScepCertificateProfile',
+            '#microsoft.graph.macOSTrustedRootCertificate',
+            '#microsoft.graph.macOSSoftwareUpdateConfiguration'
         )
 
         $iOS = @(
-            "#microsoft.graph.iosUpdateConfiguration",
-            "#microsoft.graph.iosGeneralDeviceConfiguration",
-            "#microsoft.graph.iosTrustedRootCertificate",
-            "#microsoft.graph.iosScepCertificateProfile",
-            "#microsoft.graph.iosEnterpriseWiFiConfiguration",
-            "#microsoft.graph.iosEasEmailProfileConfiguration",
-            "#microsoft.graph.iosMobileAppConfiguration"
+            '#microsoft.graph.iosUpdateConfiguration',
+            '#microsoft.graph.iosGeneralDeviceConfiguration',
+            '#microsoft.graph.iosTrustedRootCertificate',
+            '#microsoft.graph.iosScepCertificateProfile',
+            '#microsoft.graph.iosEnterpriseWiFiConfiguration',
+            '#microsoft.graph.iosEasEmailProfileConfiguration',
+            '#microsoft.graph.iosMobileAppConfiguration'
         )
 
         $Android = @(
-            "#microsoft.graph.androidDeviceOwnerEnterpriseWiFiConfiguration",
-            "#microsoft.graph.androidWorkProfileGeneralDeviceConfiguration",
-            "#microsoft.graph.androidDeviceOwnerGeneralDeviceConfiguration",
-            "#microsoft.graph.androidGeneralDeviceConfiguration",
-            "#microsoft.graph.androidDeviceOwnerScepCertificateProfile",
-            "#microsoft.graph.androidWorkProfileNineWorkEasConfiguration",
-            "#microsoft.graph.androidDeviceOwnerTrustedRootCertificate",
-            "#microsoft.graph.androidManagedStoreAppConfiguration"
+            '#microsoft.graph.androidDeviceOwnerEnterpriseWiFiConfiguration',
+            '#microsoft.graph.androidWorkProfileGeneralDeviceConfiguration',
+            '#microsoft.graph.androidDeviceOwnerGeneralDeviceConfiguration',
+            '#microsoft.graph.androidGeneralDeviceConfiguration',
+            '#microsoft.graph.androidDeviceOwnerScepCertificateProfile',
+            '#microsoft.graph.androidWorkProfileNineWorkEasConfiguration',
+            '#microsoft.graph.androidDeviceOwnerTrustedRootCertificate',
+            '#microsoft.graph.androidManagedStoreAppConfiguration'
         )
     
 
@@ -72,18 +72,18 @@ function Get-IntuneConfigProfile {
 
         foreach ($value in $values.value) {
             switch ($Platform) {
-                { $Windows -contains $value."@odata.type" } { $Platform = "Windows" }
-                { $macOS -contains $value."@odata.type" } { $Platform = "macOS" }
-                { $iOS -contains $value."@odata.type" } { $Platform = "iOS" }
-                { $Android -contains $value."@odata.type" } { $Platform = "Android" }
+                { $Windows -contains $value.'@odata.type' } { $Platform = 'Windows' }
+                { $macOS -contains $value.'@odata.type' } { $Platform = 'macOS' }
+                { $iOS -contains $value.'@odata.type' } { $Platform = 'iOS' }
+                { $Android -contains $value.'@odata.type' } { $Platform = 'Android' }
             } 
             if ($value.Name) {
                 [PSCustomObject]@{
                     DisplayName  = $value.Name
                     id           = $value.id
-                    "Created On" = $value.createdDateTime
+                    'Created On' = $value.createdDateTime
                     Assignments  = $value.Assignments
-                    Platform = $Platform 
+                    Platform     = $Platform 
 
                 }
             }
@@ -91,9 +91,9 @@ function Get-IntuneConfigProfile {
                 [PSCustomObject]@{
                     DisplayName  = $value.DisplayName
                     id           = $value.id
-                    "Created On" = $value.createdDateTime
+                    'Created On' = $value.createdDateTime
                     Assignments  = $value.Assignments
-                    Platform = $Platform 
+                    Platform     = $Platform 
 
                 }
             }
--- PowerShell.ps1/IntuneScripts/SetCertificateUserIDMapping.ps1
+++ PowerShell.ps1/IntuneScripts/SetCertificateUserIDMapping.ps1
@@ -1,111 +1,79 @@
-# Set Value
-
-function Set-CertificateUserIDMapping {
-    param (
-        [CmdletBinding()]
-        [Parameter()]
-        [string]$UserID
-    )
-
-    $module = Get-Module Microsoft.Graph.Authentication
-
-    if (-not $module) {
-        Install-Module Microsoft.Graph.Authentication -Scope CurrentUser -Force
-        Connect-MgGraph
-    }
-    else {
-        $context = Get-MgContext
-        if (-not $context) {
-            Connect-MgGraph
-        }
-    }
-  
-    $certificates = Import-CSV $path
-
-    $Issuer = "CN=ad-CA01-CA,DC=ad,DC=relrepairs,DC=com"
-    foreach ($certificate in $certificates) {
-    
-        $certificate | Where UPN -eq "$UserID"
-        $SerialNumber = $cert.SerialNumber
-        $Info = "X509:<I>$Issuer<SR>$SerialNumber"    
-
-
-        Invoke-MGGraphRequest -Method patch -Uri 'https://graph.microsoft.com/v1.0/users/$UserID/?`$select=authorizationinfo' -OutputType PSObject -Headers @{'ConsistencyLevel' = 'eventual' } -Body $params
-    }
-
-}
-
-function f1 {
+<#function Get-AllCertificateReport {
     [CmdletBinding()]
     param (
         [Parameter()]
-        [string]$UserID,
-        [switch]$Serial,
-        [switch]$SKI
+        [string]$IssuerName = 'CN=RR-SubCA, O=Reliable Repairs, L=Cleveland, S=OH, C=US'
     )
+    #$Date = (Get-Date).AddMonths(6)
+    $URI = 'https://graph.microsoft.com/beta/deviceManagement/deviceConfigurationsAllManagedDeviceCertificateStates'
+    Invoke-MgGraphRequest -Method GET -Uri $URI | Select-Object -expand Value 
+}#>
 
-    $module = Get-Module Microsoft.Graph.Authentication -ListAvailable
+function Get-IntuneCertificateReport {
+    Add-Type -AssemblyName System.Collections
+    $certificates = [System.Collections.Generic.List[PSObject]]::new()
 
-    if (-not $module) {
-        Install-Module Microsoft.Graph.Authentication -Scope CurrentUser -Force -AllowClobber
-        Connect-MgGraph
-    }
-    else {
-        $context = Get-MgContext
-        if (-not $context) {
-            Connect-MgGraph
+    # Get report for Windows SCEP Certificates
+    $FileName = 'WindowsCertReport.zip'
+
+    $URI = "https://graph.microsoft.com/beta/deviceManagement/reports/exportJobs('CertificatesByRAPolicy_83d72540-0481-42d9-b56c-401a954b0d77')"
+
+    $value = Invoke-MgGraphRequest -Method GET -Uri $URI
+
+    try {
+        Invoke-RestMethod -Uri $value.url -OutFile $FileName -ErrorAction Stop
+        try {
+            Expand-Archive -Path $FileName -DestinationPath $FileName.Replace('.zip', '') -ErrorAction Stop
+            $reportCSV = Get-ChildItem $FileName.Replace('.zip', '') -Filter '*.csv'
+            $windowsCertificateReport = Import-Csv $reportCSV
+
+        }
+        catch {
+            Write-Host 'Error extracting the file'
+            break
         }
     }
-
-    #$ID = Invoke-MgGraphRequest -uri "https://graph.microsoft.com/v1.0/users/$UserID" | Select -expand Id
-    if ($Serial) {
-        $certificates = Import-CSV $path
-        $Issuer = "DC=com,DC=relrepairs,DC=ad,CN=ad-CA01-CA"
-        $cert = @()
-        foreach ($certificate in $certificates) {
-            $cert += $certificate | Where-Object UPN -eq "$UserID"
-        }
-        $SerialNumber = $cert | Where-Object SerialNumber | Select-Object -ExpandProperty SerialNumber
-        $Info = @()
-        foreach ($serial in $SerialNumber) {
-            $Info += "X509:<I>$Issuer<SR>$serial" 
-        }
-    
-        $params = @{
-            authorizationInfo = @{
-                certificateUserIds = @(
-                    foreach ($item in $info) {
-                        $item
-                    }   
-                )
-            }
-        }
+    catch {
+        Write-Host 'Error downloading the file'
+        break
     }
 
-    if ($SKI) {
-        
-        $mycert = ls cert:\CurrentUser\My\ | Where Subject -like "*E=$UserID*"
-        $SubjectKeyID = $mycert.Extensions | Where { $_.Oid.FriendlyName -eq "Subject Key Identifier" }  | Select -expand SubjectKeyIdentifier    
+    # Get report for macOS SCEP Certificates
 
-        $params = @{
-            authorizationInfo = @{
-                certificateUserIds = @(
-                    "X509:<SKI>$SubjectKeyID"
-                )
-            }
+    $FileName = 'MacOSCertReport.zip'
+    $URI = "https://graph.microsoft.com/beta/deviceManagement/reports/exportJobs('CertificatesByRAPolicy_d413727c-94d9-4565-b1a6-990df14daeb6')"
+    $value = Invoke-MgGraphRequest -Method GET -Uri $URI
+    try {
+        Invoke-RestMethod -Uri $value.url -OutFile $FileName -ErrorAction Stop
+        try {
+            Expand-Archive -Path $FileName -DestinationPath $FileName.Replace('.zip', '') -ErrorAction Stop
+            $reportCSV = Get-ChildItem $FileName.Replace('.zip', '') -Filter '*.csv'
+            $macOSCertificateReport = Import-Csv $reportCSV
+        }
+        catch {
+            Write-Host 'Error extracting the file'
+            break
         }
     }
+    catch {
+        Write-Host 'Error downloading the file'
+        break
+    }
 
-    Invoke-MgGraphRequest -Method PATCH -Uri "https://graph.microsoft.com/v1.0/users/$UserID" -Body $params
-    Get-EntraAuthorizationInfo -EntraUser $UserID
-    
-}
+    # Compile composiste list
+    $certificates.add($windowsCertificateReport)
+    $certificates.ADd($macCertificateReport)
 
+    $certificates
 
+    Write-Verbose 'Keep downloaded files? (Y/N)' -Verbose
+    $input = Read-Host
+    if ($input -eq 'N') {
+        Remove-Item *$FileName* -Force
+        Remove-Item *macOSFileName* -Force 
+    }
 
-
-
-function Get-EntraAuthorizationInfo {
+}function Get-EntraAuthorizationInfo {
     [CmdletBinding()]
     param (
         [Parameter()]
@@ -125,7 +93,95 @@ function Get-EntraAuthorizationInfo {
         }
     }
 
-    Invoke-MGGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$EntraUser/?`$select=authorizationinfo" | Select -expand authorizationInfo | Select -expand certificateUserIds
+    Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$EntraUser/?`$select=authorizationinfo" | Select-Object -expand authorizationInfo | Select-Object -expand certificateUserIds
 }
 
 
+function Set-CertificateUserIDMapping {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$UserID,
+        [switch]$Serial,
+        [switch]$SKI,
+        [switch]$SHA1,
+        [string]$path
+    )
+
+    $module = Get-Module Microsoft.Graph.Authentication -ListAvailable
+
+    if (-not $module) {
+        Install-Module Microsoft.Graph.Authentication -Scope CurrentUser -Force -AllowClobber
+        Connect-MgGraph
+    }
+    else {
+        $context = Get-MgContext
+        if (-not $context) {
+            Connect-MgGraph
+        }
+    }
+
+    <#$ID = Invoke-MgGraphRequest -uri "https://graph.microsoft.com/v1.0/users/$UserID" | Select -expand Id
+    if ($Serial) {
+        $certificates = Get-CertificateReport
+        $Issuer = $certificates.IssuerName
+        $cert = @()
+        foreach ($certificate in $certificates) {
+            $cert += $certificate | Where-Object UPN -EQ "$UserID"
+        }
+        $SerialNumber = $cert | Where-Object SerialNumber | Select-Object -ExpandProperty SerialNumber
+        $Info = @()
+        foreach ($serialno in $SerialNumber) {
+            $Info += "X509:<I>$Issuer<SR>$serialno" 
+        }
+    
+        $params = @{
+            authorizationInfo = @{
+                certificateUserIds = @(
+                    foreach ($item in $info) {
+                        $item
+                    }   
+                )
+            }
+        }
+    }#>
+
+    if ($SKI) {
+        $mycert = Get-hildItem cert:\CurrentUser\My\ | Where-Object Subject -Like "*E=$UserID*"
+        $SubjectKeyID = $mycert.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Subject Key Identifier' } | Select-Object -expand SubjectKeyIdentifier    
+
+        $params = @{
+            authorizationInfo = @{
+                certificateUserIds = @(
+                    "X509:<SKI>$SubjectKeyID"
+                )
+            }
+        }
+    }
+
+    if ($SHA1) {
+        #$certificates = Get-CertificateReport | Where-Object userPrincipalName -EQ "$UserID"
+        #$Thumbprints = $certificates.certificateThumbprint
+        $certificates = Get-IntuneCertificateReport
+        $Thumbprints = $certificates.Thumbprint
+
+        $info = [System.Collections.Generic.List[string]]::new()
+        foreach ($thumbprint in $Thumbprints) {
+            $info.add("X509:<SHA1-PUKEY>$thumbprint")
+        }
+
+        $params = @{
+            authorizationInfo = @{
+                certificateUserIds = @(
+                    $Info               
+                )
+            }
+        } 
+    
+
+        Invoke-MgGraphRequest -Method PATCH -Uri "https://graph.microsoft.com/v1.0/users/$UserID" -Body $params -Headers @{'ConsistencyLevel' = 'eventual' } -OutputType PSObject
+        
+        Get-EntraAuthorizationInfo -EntraUser $UserID
+    
+    }
+}
